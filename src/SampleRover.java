d66e8ef5a78f1483d8e6ee8d4686b7e943321212
broderius_mar@k2ac.local <broderius_mar@k2ac.local>
2020-01-08 11:20:00 +0100

    update changes

--- src/SampleRover.java
+++ src/SampleRover.java
@@ -10,36 +10,72 @@ public class SampleRover extends Creature {
 	public static final String AUTHOR = "Mark Broderius";
 	public static final String DESCRIPTION = "A sample rover to explore the features of the simulation.";
 
-	private int height;
-	private int width;
-	private int size;
-	private Observation[] observations;
-	private boolean[] visitedPositions;
+	private GameMap map;
 
 	@Override
 	public void run() {
-		
+
 		setUp();
 		while (true) {
-			updateObservations(observe());
+			map.updateObservations(observe());
 			if (!moveForward()) {
 				attack();
 				turnLeft();
 			}
 		}
 	}
-	
-	private void updateObservations(Observation[] observations) {
-		
-	}
 
 	private void setUp() {
-		Dimension dimension = getMapDimensions();
-		height = (int) dimension.getHeight();
-		width = (int) dimension.getWidth();
-		size = height * width;
-		observations = new Observation[size];
-		visitedPositions = new boolean[size];
+		map = new GameMap(getMapDimensions());
+	}
+
+	public static class GameMap {
+
+		private int height;
+		private int width;
+		private int size;
+		private GameField[] fields;
+
+		public GameMap(Dimension dimension) {
+			height = (int) dimension.getHeight();
+			width = (int) dimension.getWidth();
+			size = height * width;
+			fields = new GameField[size];
+		}
+
+		public void updateObservations(Observation[] observations) {
+			for (Observation observation : observations) {
+				updateObservation(observation);
+			}
+		}
+
+		private void updateObservation(Observation observation) {
+			observation.position.
+		}
+
+		public int position(int xCoord, int yCoord) {
+			return xCoord * width + yCoord;
+		}
+
+		public int xCoord(int position) {
+			return position / width;
+		}
+
+		public int yCoord(int position) {
+			return position % width;
+		}
+
+	}
+
+	public static class GameField {
+
+		private Observation observation;
+		private boolean visited;
+
+		public GameField() {
+
+		}
+
 	}
 
 	@Override
