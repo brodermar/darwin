fad5a1f36a7e0f18d20bfa21438cc6f247b743a6
MarkB <mark.broderius@yahoo.de>
2020-01-21 22:11:27 +0100

    improved the sample rover

--- src/SampleRover.java
+++ src/SampleRover.java
@@ -1,5 +1,7 @@
 import java.awt.Dimension;
 import java.awt.Point;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * A sample rover to explore the features of the simulation.
@@ -13,23 +15,40 @@ public class SampleRover extends Creature {
 
 	private static GameMap map;
 
+	private Trace trace;
+
 	@Override
 	public void run() {
 		setUp();
 		while (true) {
-			map.updateObservations(observe());
 			System.out.println(map.print());
-			if (!moveForward()) {
+			Observation[] observations = observe();
+			map.updateObservations(observations);
+			if (trace.isVisited(observations[1].position)) {
+				turnLeft();
+			}
+			if (!moveForwardAdvanced()) {
 				attack();
 				turnLeft();
 			}
 		}
 	}
 
+	boolean moveForwardAdvanced() {
+		boolean returnVal = moveForward();
+		if (returnVal) {
+			trace.setVisited(getPosition());
+			map.updateObservation(new Observation(trace.getLastPosition(), getGameTime()));
+		}
+		return returnVal;
+	}
+
 	private void setUp() {
 		if (map == null) {
 			map = new GameMap(getMapDimensions());
 		}
+		trace = new Trace(getMapDimensions(), getId());
+		trace.setVisited(getPosition());
 	}
 
 	public static class GameMap {
@@ -55,7 +74,7 @@ public class SampleRover extends Creature {
 			}
 		}
 
-		private void updateObservation(Observation observation) {
+		public void updateObservation(Observation observation) {
 			GameField field = getField(observation.position);
 			field.updateObservation(observation);
 		}
@@ -80,13 +99,30 @@ public class SampleRover extends Creature {
 	public static class GameField {
 
 		private Observation observation;
+		private Symbol symbol;
 
 		public GameField() {
-
+			symbol = Symbol.EMPTY;
 		}
 
 		private void updateObservation(Observation observation) {
 			this.observation = observation;
+			switch (observation.type) {
+			case CREATURE:
+				setSymbol(Symbol.CREATURE);
+				break;
+			case EMPTY:
+				setSymbol(Symbol.EMPTY);
+				break;
+			case HAZARD:
+				setSymbol(Symbol.HAZARD);
+				break;
+			case WALL:
+				setSymbol(Symbol.WALL);
+				break;
+			default:
+				throw new NullPointerException("cannot identify type");
+			}
 		}
 
 		private Observation getObservation() {
@@ -94,21 +130,65 @@ public class SampleRover extends Creature {
 		}
 
 		public char print() {
-			if (observation == null) {
-				return ' ';
+			return symbol.getSymbol();
+		}
+
+		public void setSymbol(Symbol symbol) {
+			this.symbol = symbol;
+		}
+
+	}
+
+	public static enum Symbol {
+
+		CREATURE('c'), EMPTY(' '), HAZARD('h'), WALL('X');
+
+		private char symbol;
+
+		private Symbol(char symbol) {
+			this.symbol = symbol;
+		}
+
+		public char getSymbol() {
+			return symbol;
+		}
+	}
+
+	public static class Trace {
+
+		int id;
+		private boolean[][] fields;
+		private Point currentPosition;
+		private Point lastPosition;
+
+		public Trace(Dimension dimension, int id) {
+			this.id = id;
+			int height = (int) dimension.getHeight();
+			int width = (int) dimension.getWidth();
+			fields = new boolean[width][height];
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					fields[i][j] = false;
+				}
 			}
-			switch (observation.type) {
-			case CREATURE:
-				return 'c';
-			case EMPTY:
-				return ' ';
-			case HAZARD:
-				return 'h';
-			case WALL:
-				return 'X';
-			default:
-				throw new NullPointerException("cannot identify type");
-			}
+		}
+
+		public boolean isVisited(Point position) {
+			return fields[position.y - 1][position.x - 1];
+		}
+
+		public void setVisited(Point position) {
+			fields[position.y - 1][position.x - 1] = true;
+			lastPosition = currentPosition;
+			currentPosition = position;
+		}
+
+		public Point getCurrentPosition() {
+			return currentPosition;
+		}
+
+		public Point getLastPosition() {
+			return lastPosition;
 		}
 
 	}
