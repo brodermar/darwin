e69d26379b27052a2dbb4f254287a604a5a754e7
broderius_mar@k2ac.local <broderius_mar@k2ac.local>
2020-01-30 12:25:13 +0100

    added comments to the existing code

--- src/SampleRover.java
+++ src/SampleRover.java
@@ -58,14 +58,23 @@ public class SampleRover extends Creature {
 		}
 	}
 
-	public GameField current() {
+	// returns the game field of the map of the current position
+	public GameField getCurrentGameField() {
 		return map.getField(getPosition());
 	}
 
-	public GameField last() {
+	// returns the game field of the map of the last position
+	public GameField getLastGameField() {
 		return trace.getLastPosition() != null ? map.getField(trace.getLastPosition()) : null;
 	}
 
+	// updates the trace with the current position
+	public void updateTrace() {
+		trace.updatePosition(getPosition());
+	}
+
+	// returns the neighbour GameField of the given direction and the current
+	// position
 	public GameField neighbour(Direction targetDirection) {
 		switch (targetDirection) {
 		case EAST:
@@ -81,18 +90,22 @@ public class SampleRover extends Creature {
 		}
 	}
 
+	// returns the GameField in the front of the rover
 	public GameField front() {
 		return neighbour(getDirection());
 	}
 
+	// returns the GameField to the left of the rover
 	public GameField left() {
 		return neighbour(getDirection().left());
 	}
 
+	// returns the GameField to the right of the rover
 	public GameField right() {
 		return neighbour(getDirection().right());
 	}
 
+	// returns the GameField in the back of the rover
 	public GameField back() {
 		return neighbour(getDirection().opposite());
 	}
@@ -100,18 +113,20 @@ public class SampleRover extends Creature {
 	boolean moveForwardAdvanced() {
 		boolean returnVal = moveForward();
 		if (returnVal) {
-			trace.setVisited(getPosition());
+			updateTrace();
 			map.updateObservation(new Observation(trace.getLastPosition(), getGameTime()));
 		}
 		return returnVal;
 	}
 
+	// initializes a new map if necessary, creates a new trace for this creature and
+	// updates it the first time
 	private void setUp() {
 		if (map == null) {
 			map = new GameMap(getMapDimensions());
 		}
 		trace = new Trace(getMapDimensions(), getId());
-		trace.setVisited(getPosition());
+		updateTrace();
 	}
 
 	public static class GameMap {
@@ -120,6 +135,7 @@ public class SampleRover extends Creature {
 		private int width;
 		private GameField[][] fields;
 
+		// constructs a new game map: initializes the game fields
 		public GameMap(Dimension dimension) {
 			height = (int) dimension.getHeight();
 			width = (int) dimension.getWidth();
@@ -131,12 +147,15 @@ public class SampleRover extends Creature {
 			}
 		}
 
+		// updates matching fields with the new observations
 		public void updateObservations(Observation[] observations) {
 			for (Observation observation : observations) {
 				updateObservation(observation);
 			}
 		}
 
+		// updates the field of the position given by the observation with the
+		// observation
 		public void updateObservation(Observation observation) {
 			GameField field = getField(observation.position);
 			field.updateObservation(observation);
@@ -146,27 +165,36 @@ public class SampleRover extends Creature {
 			return fields[position.x][position.y];
 		}
 
+		// returns the field above the given position or null: decrements the
+		// x-coordinate if possible
 		public GameField top(Point position) {
 			return position.x > 0 ? fields[position.y][position.x - 1] : null;
 		}
 
+		// returns the field under the given position or null: increments the
+		// x-coordinate if possible
 		public GameField bottom(Point position) {
-			return position.x < height - 1 ? fields[position.y - 1][position.x + 1] : null;
+			return position.x < height - 1 ? fields[position.y][position.x + 1] : null;
 		}
 
+		// returns the field on the left side of the given position or null: decrements
+		// the y-coordinate if possible
 		public GameField left(Point position) {
-			return position.y > 0 ? fields[position.y][position.x] : null;
+			return position.y > 0 ? fields[position.y - 1][position.x] : null;
 		}
 
+		// return the field right to the given position or null: increments the
+		// y-coordinate if possible
 		public GameField right(Point position) {
 			return position.y < width - 1 ? fields[position.y + 1][position.x] : null;
 		}
 
+		// prints the map: returns a Strings
 		public String print() {
 			StringBuilder builder = new StringBuilder();
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-					builder.append(fields[i][j].print());
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
+					builder.append(fields[j][i].print());
 				}
 				builder.append('\n');
 			}
@@ -180,10 +208,13 @@ public class SampleRover extends Creature {
 		private Observation observation;
 		private Symbol symbol;
 
+		// constructs a new game field with the symbol UNKNOWN
 		public GameField() {
 			symbol = Symbol.UNKOWN;
 		}
 
+		// sets the observation of the field and updates the field symbol depending on
+		// the typ of the observation
 		private void updateObservation(Observation observation) {
 			this.observation = observation;
 			switch (observation.type) {
@@ -204,22 +235,27 @@ public class SampleRover extends Creature {
 			}
 		}
 
+		// observation != null type == WALL
 		public boolean isWall() {
 			return observation != null ? observation.type.equals(Type.WALL) : false;
 		}
 
+		// observation != null type == CREATURE
 		public boolean isCreature() {
 			return observation != null ? observation.type.equals(Type.CREATURE) : false;
 		}
 
+		// observation != null type == HAZARD
 		public boolean isHazard() {
 			return observation != null ? observation.type.equals(Type.HAZARD) : false;
 		}
 
+		// observation != null && type == EMPTY
 		public boolean isEmpty() {
 			return observation != null ? observation.type.equals(Type.EMPTY) : false;
 		}
 
+		// observation != null
 		public boolean isUnknown() {
 			return observation == null;
 		}
@@ -228,14 +264,17 @@ public class SampleRover extends Creature {
 			return observation;
 		}
 
+		// returns the symbol determinded by updateObservation(..)
 		public char print() {
 			return symbol.getSymbol();
 		}
 
-		public void setSymbol(Symbol symbol) {
+		// sets the symbol of this field
+		private void setSymbol(Symbol symbol) {
 			this.symbol = symbol;
 		}
 
+		// returns the position of the observation
 		public Point getPosition() {
 			return observation.position;
 		}
@@ -264,6 +303,7 @@ public class SampleRover extends Creature {
 		private Point currentPosition;
 		private Point lastPosition;
 
+		// constructs and initalizes a new trace for a creature id
 		public Trace(Dimension dimension, int id) {
 			this.id = id;
 			int height = (int) dimension.getHeight();
@@ -276,20 +316,27 @@ public class SampleRover extends Creature {
 			}
 		}
 
+		// returns the value of the field of the given position
 		public boolean isVisited(Point position) {
 			return fields[position.x][position.y];
 		}
 
-		public void setVisited(Point position) {
+		// sets the field of the given position to true, updates the current position to
+		// the given one and sets the old position to the field "lastPosition"
+		public void updatePosition(Point position) {
 			fields[position.x][position.y] = true;
 			lastPosition = currentPosition;
 			currentPosition = position;
 		}
 
+		// returns the current position, this means the last position added by
+		// "setCurrentPosition()"
 		public Point getCurrentPosition() {
 			return currentPosition;
 		}
 
+		// returns the last position, this means the last before position before the
+		// last position added by "setCurrentPosition()"
 		public Point getLastPosition() {
 			return lastPosition;
 		}
