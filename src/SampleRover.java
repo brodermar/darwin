edcae0d13665d51647c3db362e6de503525a694c
broderius_mar@k2ac.local <broderius_mar@k2ac.local>
2020-02-19 10:27:17 +0100

    copied graph and dijkstra into sample rover

--- src/SampleRover.java
+++ src/SampleRover.java
@@ -1,8 +1,15 @@
 import java.awt.Dimension;
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -505,6 +512,375 @@ public class SampleRover extends Creature {
 
 		int getVisitedTimes() {
 			return visitedTimes;
+		}
+
+	}
+	
+	public interface RoutableVertex<E> {
+		
+		E getElement();
+
+		boolean isVisited();
+
+		void setVisited(boolean visited);
+
+		double getDistance();
+
+		double setDistance(double distance);
+
+		RoutableVertex<E> getPredecessor();
+
+		RoutableVertex<E> setPredecessor(RoutableVertex<E> vertex);
+		
+	}
+	
+	public interface RoutableGraph<E> {
+
+		RoutableVertex<E> addVertex(E element);
+
+		RoutableVertex<E> removeVertex(RoutableVertex<E> vertex);
+
+		Set<RoutableVertex<E>> getVertices();
+
+		boolean contains(RoutableVertex<E> vertex);
+
+		boolean contains(RoutableEdge<E> edge);
+
+		RoutableEdge<E> addEdge(RoutableVertex<E> firstVertex, RoutableVertex<E> secondVertex, double weight);
+
+		RoutableEdge<E> removeEdge(RoutableEdge<E> edge);
+
+		List<RoutableEdge<E>> getEdges(RoutableVertex<E> vertex);
+
+		Set<RoutableEdge<E>> getEdges();
+
+	}
+	
+	public interface RoutableEdge<E> {
+
+		double getWeight();
+
+		double setWeight(double weight);
+
+		RoutableVertex<E> getFirstVertex();
+
+		RoutableVertex<E> getSecondVertex();
+
+		RoutableVertex<E> getOtherVertex(RoutableVertex<E> vertex);
+
+	}
+	
+	public class RoutableGraphImpl<E> implements RoutableGraph<E> {
+
+		private Map<RoutableVertexImpl<E>, List<RoutableEdgeImpl<E>>> vertices;
+
+		public RoutableGraphImpl() {
+			vertices = new HashMap<>();
+		}
+
+		@Override
+		public RoutableVertex<E> addVertex(E element) {
+			RoutableVertexImpl<E> vertex = new RoutableVertexImpl<>(element);
+			vertices.put(vertex, new ArrayList<RoutableEdgeImpl<E>>());
+			return vertex;
+		}
+
+		@Override
+		public RoutableVertex<E> removeVertex(RoutableVertex<E> vertex) {
+			RoutableVertexImpl<E> vertexImpl = validate(vertex);
+			List<RoutableEdgeImpl<E>> edges = vertices.remove(vertexImpl);
+			for (RoutableEdgeImpl<E> edge : edges) {
+				RoutableVertexImpl<E> neighbourVertex = edge.getOtherVertexImpl(vertexImpl);
+				vertices.get(neighbourVertex).remove(edge);
+			}
+			return vertexImpl;
+		}
+
+		@Override
+		public Set<RoutableVertex<E>> getVertices() {
+			Set<RoutableVertex<E>> set = new HashSet<>();
+			set.addAll(vertices.keySet());
+			return set;
+		}
+
+		@Override
+		public RoutableEdge<E> addEdge(RoutableVertex<E> firstVertex, RoutableVertex<E> secondVertex, double weight) {
+			RoutableVertexImpl<E> first = validate(firstVertex);
+			RoutableVertexImpl<E> second = validate(secondVertex);
+			RoutableEdgeImpl<E> newEdge = new RoutableEdgeImpl<E>(first, second, weight);
+			vertices.get(first).add(newEdge);
+			vertices.get(second).add(newEdge);
+			return newEdge;
+		}
+
+		@Override
+		public RoutableEdge<E> removeEdge(RoutableEdge<E> edge) {
+			RoutableEdgeImpl<E> edgeImpl = validate(edge);
+			RoutableVertexImpl<E> firstVertex = edgeImpl.getFirstVertexImpl();
+			RoutableVertexImpl<E> secondVertex = edgeImpl.getSecondVertexImpl();
+			vertices.get(firstVertex).remove(edgeImpl);
+			vertices.get(secondVertex).remove(edgeImpl);
+			return edgeImpl;
+		}
+
+		@Override
+		public Set<RoutableEdge<E>> getEdges() {
+			Set<RoutableEdge<E>> set = new HashSet<>();
+			for (List<RoutableEdgeImpl<E>> edges : vertices.values()) {
+				set.addAll(edges);
+			}
+			return set;
+		}
+
+		@Override
+		public List<RoutableEdge<E>> getEdges(RoutableVertex<E> vertex) {
+			RoutableVertexImpl<E> vertexImpl = validate(vertex);
+			return new ArrayList<RoutableEdge<E>>(vertices.get(vertexImpl));
+		}
+
+		@Override
+		public boolean contains(RoutableVertex<E> vertex) {
+			return vertex instanceof RoutableVertexImpl;
+		}
+
+		@Override
+		public boolean contains(RoutableEdge<E> edge) {
+			return edge instanceof RoutableEdgeImpl;
+		}
+
+		private RoutableVertexImpl<E> validate(RoutableVertex<E> vertex) {
+			if (vertex instanceof RoutableVertexImpl) {
+				return (RoutableVertexImpl<E>) vertex;
+			} else {
+				throw new IllegalArgumentException("the given vertex has no valid type");
+			}
+		}
+
+		private RoutableEdgeImpl<E> validate(RoutableEdge<E> edge) {
+			if (edge instanceof RoutableEdgeImpl) {
+				return (RoutableEdgeImpl<E>) edge;
+			} else {
+				throw new IllegalArgumentException("the given edge has no valid type");
+			}
+		}
+
+		private class RoutableEdgeImpl<T> implements RoutableEdge<T> {
+
+			private double weight;
+			private RoutableVertexImpl<T> firstVertex;
+			private RoutableVertexImpl<T> secondVertex;
+
+			private RoutableEdgeImpl(RoutableVertexImpl<T> firstVertex, RoutableVertexImpl<T> secondVertex, double weight) {
+				this.firstVertex = firstVertex;
+				this.secondVertex = secondVertex;
+				this.weight = weight;
+			}
+
+			@Override
+			public double getWeight() {
+				return weight;
+			}
+
+			@Override
+			public double setWeight(double weight) {
+				double old = this.weight;
+				this.weight = weight;
+				return old;
+			}
+
+			private RoutableVertexImpl<T> getOtherVertexImpl(RoutableVertexImpl<T> vertex) {
+				if (firstVertex.equals(vertex)) {
+					return secondVertex;
+				} else if (secondVertex.equals(vertex)) {
+					return firstVertex;
+				} else {
+					throw new IllegalArgumentException("the edge contains not the given vertex");
+				}
+			}
+
+			private RoutableVertexImpl<T> getFirstVertexImpl() {
+				return firstVertex;
+			}
+
+			private RoutableVertexImpl<T> getSecondVertexImpl() {
+				return secondVertex;
+			}
+
+			@Override
+			public RoutableVertex<T> getFirstVertex() {
+				return firstVertex;
+			}
+
+			@Override
+			public RoutableVertex<T> getSecondVertex() {
+				return secondVertex;
+			}
+
+			@Override
+			public String toString() {
+				StringBuilder builder = new StringBuilder();
+				builder.append("RoutableEdgeImpl [weight=");
+				builder.append(weight);
+				builder.append(", firstVertex=");
+				builder.append(firstVertex);
+				builder.append(", secondVertex=");
+				builder.append(secondVertex);
+				builder.append("]");
+				return builder.toString();
+			}
+
+			@Override
+			public RoutableVertex<T> getOtherVertex(RoutableVertex<T> vertex) {
+				if (getFirstVertex().equals(vertex)) {
+					return secondVertex;
+				} else if (getSecondVertex().equals(vertex)) {
+					return firstVertex;
+				} else {
+					throw new IllegalArgumentException("the edge contains not the given vertex");
+				}
+			}
+
+		}
+
+		private class RoutableVertexImpl<T> implements RoutableVertex<T> {
+
+			private RoutableVertex<T> predecessor;
+			private double distance;
+			private boolean visited;
+			private T element;
+
+			private RoutableVertexImpl(T element) {
+				this.element = element;
+				this.distance = Double.MAX_VALUE;
+				this.visited = false;
+			}
+
+			@Override
+			public T getElement() {
+				return element;
+			}
+
+			@Override
+			public boolean isVisited() {
+				return visited;
+			}
+
+			@Override
+			public void setVisited(boolean visited) {
+				this.visited = visited;
+			}
+
+			@Override
+			public double getDistance() {
+				return distance;
+			}
+
+			@Override
+			public double setDistance(double distance) {
+				double old = this.distance;
+				this.distance = distance;
+				return old;
+			}
+
+			@Override
+			public RoutableVertex<T> getPredecessor() {
+				return predecessor;
+			}
+
+			@Override
+			public RoutableVertex<T> setPredecessor(RoutableVertex<T> vertex) {
+				RoutableVertex<T> old = predecessor;
+				predecessor = vertex;
+				return old;
+			}
+
+			@Override
+			public String toString() {
+				StringBuilder builder = new StringBuilder();
+				builder.append("RoutableVertexImpl [distance=");
+				builder.append(distance);
+				builder.append(", visited=");
+				builder.append(visited);
+				builder.append(", element=");
+				builder.append(element);
+				builder.append("]");
+				return builder.toString();
+			}
+
+		}
+
+	}
+	
+	public class Dijkstra<E> {
+
+		private RoutableGraph<E> graph;
+		private RoutableVertex<E> start;
+		private RoutableVertex<E> target;
+		private PriorityQueue<RoutableVertex<E>> queue;
+
+		public Dijkstra() {
+			queue = new PriorityQueue<RoutableVertex<E>>(new RoutableVertexComparator());
+		}
+
+		private void calcShortestPath() {
+			for (RoutableVertex<E> vertex : graph.getVertices()) {
+				vertex.setDistance(Double.MAX_VALUE);
+				vertex.setVisited(false);
+			}
+			start.setDistance(0.0);
+			queue.clear();
+			queue.add(start);
+			double distance;
+			while (!queue.isEmpty()) {
+				RoutableVertex<E> curVertex = queue.poll();
+				if (target != null && target.equals(curVertex)) {
+					break;
+				}
+				curVertex.setVisited(true);
+				List<RoutableEdge<E>> edges = graph.getEdges(curVertex);
+				for (RoutableEdge<E> edge : edges) {
+					RoutableVertex<E> neighbourVertex = edge.getOtherVertex(curVertex);
+					if (!neighbourVertex.isVisited()) {
+						distance = curVertex.getDistance() + edge.getWeight();
+						if (distance < neighbourVertex.getDistance()) {
+							neighbourVertex.setDistance(distance);
+							neighbourVertex.setPredecessor(curVertex);
+						}
+						queue.add(neighbourVertex);
+					}
+				}
+			}
+		}
+
+		public RoutableVertex<E> calcShortestPath(RoutableGraph<E> graph, RoutableVertex<E> start,
+				RoutableVertex<E> target) {
+			initialize(graph, start, target);
+			calcShortestPath();
+			return target;
+		}
+
+		private void initialize(RoutableGraph<E> graph, RoutableVertex<E> start, RoutableVertex<E> target) {
+			if (start == null) {
+				throw new NullPointerException("start is null");
+			}
+			if (target == null) {
+				throw new NullPointerException("target is null");
+			}
+			if (!graph.contains(start) || !graph.contains(target)) {
+				throw new IllegalArgumentException("graph does not contain start or target");
+			}
+			this.graph = graph;
+			this.start = start;
+			this.target = target;
+		}
+
+		private class RoutableVertexComparator implements Comparator<RoutableVertex<E>> {
+
+			@Override
+			public int compare(RoutableVertex<E> o1, RoutableVertex<E> o2) {
+				return Double.compare(o1.getDistance(), o2.getDistance());
+			}
+
 		}
 
 	}
